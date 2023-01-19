cffe8cf909298c36cfe146dfe0c0e800d3c33628
henriklarsen28 <henrik.hl@hotmail.com>
2023-01-19 20:20:00 +0100

    feat: Saving files to local disk

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bachelor_backend.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bachelor_backend.Controller
 {
+    [Route("[controller]/[action]")]
     public class UserController : ControllerBase
     {
-        
+
+        private readonly IVoiceRepository _db;
+
+        public UserController(IVoiceRepository db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> SaveFile(IFormFile recording)
+        {
+
+            string uuid = await _db.SaveFile(recording);
+            if (uuid.Equals(null))
+            {
+                return BadRequest("Voice recording is not saved");
+            }
+            return Ok(uuid);
+            
+        }
+
     }
 }
---
+++ DAL/VoiceRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace Bachelor_backend.DAL;
+
+public class VoiceRepository : IVoiceRepository
+{
+    
+    // https://learn.microsoft.com/en-us/dotnet/api/system.guid.newguid?view=net-6.0
+    public async Task<string> SaveFile(IFormFile recording)
+    {
+        try
+        {
+            //Creates a recording directory if it doesnt exist
+            if (!Directory.Exists($@"{Directory.GetCurrentDirectory()}\recordings"))
+            {
+                Directory.CreateDirectory($@"{Directory.GetCurrentDirectory()}\recordings");
+            }
+            
+            // Creates a new file name with a uuid name
+            string uuid = Guid.NewGuid().ToString();
+            string extension = Path.GetExtension(recording.FileName);
+            
+            string newFileName = uuid + extension;
+            string uploadPath = Path.Combine($@"{Directory.GetCurrentDirectory()}\recordings", newFileName);
+            
+            //TODO: Insert uuid and file path in db
+            
+            //Using stream to copy the content of the recording file to disk
+            using (var stream = new FileStream(uploadPath, FileMode.Create))
+            {
+                await recording.CopyToAsync(stream);
+            }
+            return uuid;
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+    }
+    
+}
