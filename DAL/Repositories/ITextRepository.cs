31f0f2fb605eece3a61e1b2aedb0ba0be50603e4
Johan Hustoft Hansen <s349880@oslomet.no>
2023-01-23 12:01:16 +0100

    feat: Created text/tag functions

--- DAL/Repositories/ITextRepository.cs
+++ DAL/Repositories/ITextRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Bachelor_backend.DAL.Repositories
+﻿using Bachelor_backend.Models;
+
+namespace Bachelor_backend.DAL.Repositories
 {
     public interface ITextRepository
     {
+        Task<List<Text>> GetTexts();
+        Task<bool> CreateText();
+        Task<bool> CreateTag();
+        Task<List<Tag>> GetTags();
     }
 }
--- DAL/Repositories/TextRepository.cs
+++ DAL/Repositories/TextRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace Bachelor_backend.DAL.Repositories
+﻿using Bachelor_backend.Models;
+
+namespace Bachelor_backend.DAL.Repositories
 {
     public class TextRepository
     {
+        public async Task<List<Text>> GetTexts()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<List<Tag>> GetTags()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> CreateText()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<bool> CreateTag()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
