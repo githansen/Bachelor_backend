3f93e4869f0aa6ce41658d1f347953532559793d
henriklarsen28 <henrik.hl@hotmail.com>
2023-03-01 11:19:53 +0100

    feat: Unit test getTag and create text

--- xUnitBackendTest/AdminTest.cs
+++ xUnitBackendTest/AdminTest.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Bachelor_backend.Controller;
 using Bachelor_backend.DAL.Repositories;
+using Bachelor_backend.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,8 +12,9 @@ namespace xUnitBackendTest;
 public class AdminTest
 {
     private static readonly Mock<ITextRepository> mockTextRep = new();
+    private static readonly Mock<IVoiceRepository> mockVoiceRep = new();
     private static readonly Mock<ILogger<AdminController>> _logger = new();
-    private readonly AdminController _adminController = new(mockTextRep.Object, _logger.Object);
+    private readonly AdminController _adminController = new(mockTextRep.Object, mockVoiceRep.Object, _logger.Object);
 
 
     [Fact]
@@ -42,6 +44,78 @@ public class AdminTest
         //Assert
         Assert.Equal((int) HttpStatusCode.BadRequest, result.StatusCode);
         Assert.Equal(false, result.Value);
+    }
+
+    [Fact]
+    public async Task GetTagOk()
+    {
+        //Arrange
+        var tags = new List<Tag>
+        {
+            new Tag()
+            {
+                TagId = 1,
+                TagText= "test"
+            },
+            new Tag()
+            {
+                TagId = 2,
+                TagText= "test2"
+                }
+        };
+
+        mockTextRep.Setup(x => x.GetAllTags()).ReturnsAsync(tags);
+
+        //Act
+        var result = await _adminController.GetTags() as OkObjectResult;
+
+        //Assert
+        Assert.Equal((int) HttpStatusCode.OK, result.StatusCode);
+        Assert.Equal(tags, result.Value);
+    }
+
+    [Fact]
+    public async Task CreateTextOk()
+    {
+        //Arrange
+        var text = new Text()
+        {
+            Active = true,
+            TextId = 1,
+            TextText = "test",
+            Tags = new List<Tag>()
+            {
+                new Tag()
+                {
+                    TagId = 1,
+                    TagText = "test"
+                }
+            }
+        };
         
+        mockTextRep.Setup(x => x.CreateText(It.IsAny<Text>())).ReturnsAsync(true);
+        
+        //Act
+        var result = await _adminController.CreateText(text) as OkObjectResult;
+        
+        //Assert
+        Assert.Equal((int) HttpStatusCode.OK, result.StatusCode);
+        Assert.Equal(true, result.Value);
+    }
+    
+    [Fact]
+    public async Task CreateTextFault()
+    {
+        //Arrange
+        var text = new Text();
+
+        mockTextRep.Setup(x => x.CreateText(It.IsAny<Text>())).ReturnsAsync(false);
+        
+        //Act
+        var result = await _adminController.CreateText(text) as ObjectResult;
+        
+        //Assert
+        Assert.Equal((int) HttpStatusCode.InternalServerError, result.StatusCode);
+        Assert.Equal(false, result.Value);
     }
 }
