523dc6490bf8bdcef78a16b7598bbb683486073e
Johan Hustoft Hansen <s349880@oslomet.no>
2023-03-13 12:16:58 +0100

    fix: migration

--- Bachelor_backend/Models/AdminUser.cs
+++ Bachelor_backend/Models/AdminUser.cs
@@ -1,7 +1,10 @@
-﻿namespace Bachelor_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Bachelor_backend.Models
 {
     public class AdminUser
     {
+        [Key]
         public String Username { get; set; }
         public String Password { get; set; }
     }
